921fac78d80573e08ee53f696accf6dad5f1731a
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-06-12 15:16:49 -0300

    Add files via upload

---
+++ Lista01_LP3/Exercicio07/Exercicio07/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float fah = 0, cel = 0;
+
+            Console.WriteLine("Digite uma temperatura em grau fahrenheit : ");
+            fah = float.Parse(Console.ReadLine());
+
+            cel = 5 * (fah - 32) / 9;
+
+            Console.WriteLine("A TEMPERATURA CONVERTIDA PARA GRAUS CELSIUS É " + cel);
+        }
+    }
+}
---
+++ Lista01_LP3/Exercicio08/Exercicio08/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kel = 0, cel = 0;
+
+            Console.WriteLine("Digite uma temperatura em Kelvin : ");
+            kel = double.Parse(Console.ReadLine());
+            
+            cel = kel - 273.15;
+
+            Console.WriteLine("A TEMPERATURA CONVERTIDA EM GRAUS CELSIUS É " + cel);
+        }
+    }
+}
