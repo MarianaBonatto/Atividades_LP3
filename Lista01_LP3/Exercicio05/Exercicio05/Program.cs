bb6767ea9e337b057fd4bf520a29b1a7d397f057
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-06-12 15:15:42 -0300

    Add files via upload

---
+++ Lista01_LP3/Exercicio05/Exercicio05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num = 0, res = 0;
+
+            Console.WriteLine("Digite um número real : ");
+            num = float.Parse(Console.ReadLine());
+
+            res = num / 5;
+
+            Console.WriteLine("A QUINTA PARTE DESSE NÚMERO É " + res);
+        }
+    }
+}
---
+++ Lista01_LP3/Exercicio06/Exercicio06/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float cel = 0, fah = 0;
+
+            Console.WriteLine("Digite uma temperatura em graus celsius : ");
+            cel = float.Parse(Console.ReadLine());
+
+            fah = cel * 9/5 + 32;
+
+            Console.WriteLine("A TEMPERATURA CONVERTIDA PARA FAHRENHEIT É " + fah);
+        }
+    }
+}
