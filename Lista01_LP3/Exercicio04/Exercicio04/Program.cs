fc6fdc6de9987594440556d7e0fc034e4c6cb17d
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-06-12 15:14:42 -0300

    Add files via upload

---
+++ Lista01_LP3/Exercicio04/Exercicio04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 0, res = 0;
+
+            Console.WriteLine("Digite um número real : ");
+            num1 = float.Parse(Console.ReadLine());
+
+            res = num1*num1;
+
+            Console.WriteLine("O QUADRADO DESSE NÚMERO É " + res);
+        }
+    }
+}
