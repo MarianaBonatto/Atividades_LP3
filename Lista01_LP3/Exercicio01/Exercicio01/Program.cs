83819fd3b23e261bf7a01edd1ac0d17c97b86768
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-06-12 15:13:10 -0300

    Add files via upload

---
+++ Lista01_LP3/Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            Console.WriteLine("Digite um número inteiro : ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O NÚMERO DIGITADO PELO USUÁRIO FOI " + num);
+        }
+    }
+}
---
+++ Lista01_LP3/Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num = 0;
+
+            Console.WriteLine("Digite um número real : ");
+            num = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(" O NÚMERO DIGITADO PELO USUÁRIO FOI " + num);
+        }
+    }
+}
