6fb5ea6f148e4f435e8f6c1c3cbd126d49ad1ce5
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-06-23 14:33:21 -0300

    Add files via upload

---
+++ Lista02_LP3/Exercicio09/Exercicio09/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, soma = 0;
+
+            Console.WriteLine("DIGITE UM NÚMERO INTEIRO : ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num > 0)
+            {
+                while (num > 0)
+                {
+                    soma = soma + num % 10;
+                    num = num / 10;
+                }
+                Console.WriteLine("A SOMA DOS ALGARISMOS É " + soma);
+            }
+            else
+                Console.WriteLine("NÚMERO INVÁLIDO");
+        }
+    }
+}
---
+++ Lista02_LP3/Exercicio10/Exercicio10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, media;
+
+            Console.WriteLine("DIGITE A PRIMEIRA NOTA : ");
+            nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE A SEGUNDA NOTA : ");
+            nota2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE A TERCEIRA NOTA : ");
+            nota3 = double.Parse(Console.ReadLine());
+
+            media = ((nota1 * 1) + (nota2 * 1) + (nota3 * 2)) / 3;
+
+            if (media >= 60.0)
+                Console.WriteLine("ALUNO APROVADO COM " + media + " PONTOS");
+            else
+                Console.WriteLine("ALUNO REPROVADO COM " + media + " PONTOS");
+        }
+    }
+}
