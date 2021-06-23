8810c2730733769f1f4f9acee42df25880c4bbe9
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-06-23 14:32:19 -0300

    Add files via upload

---
+++ Lista02_LP3/Exercicio07/Exercicio07/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, prestacao;
+
+            Console.WriteLine("DIGITE O VALOR DO SALÁRIO : ");
+            salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE O VALOR DA PRESTAÇÃO : ");
+            prestacao = double.Parse(Console.ReadLine());
+
+            if(prestacao > (salario*20/100))
+                Console.WriteLine("EMPRÉSTIMO NÃO CONCEDIDO");
+            else
+                Console.WriteLine("EMPRÉSTIMO CONCEDIDO");
+        }
+    }
+}
---
+++ Lista02_LP3/Exercicio08/Exercicio08/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercicio08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura, peso;
+            char sexo;
+
+            Console.WriteLine("Digite F se você for do sexo feminino ou M se você for do sexo masculino");
+            sexo = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite sua altura : ");
+            altura = double.Parse(Console.ReadLine());
+
+            switch(sexo)
+            {
+                case 'F': 
+                    peso = (62.1 * altura) - 44.7;
+                    Console.WriteLine("O SEU PESO IDEAL É " + peso);
+                    break;
+
+                case 'M':
+                    peso = (72.7 * altura) - 58;
+                    Console.WriteLine("O SEU PESO IDEAL É " + peso);
+                    break;
+            }
+
+        }
+    }
+}
