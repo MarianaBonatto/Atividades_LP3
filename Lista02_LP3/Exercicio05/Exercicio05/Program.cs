310845467850f8c59b493cd2945a2aa2b48bba8a
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-06-23 14:31:04 -0300

    Add files via upload

---
+++ Lista02_LP3/Exercicio05/Exercicio05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("DIGITE UM NÚMERO INTEIRO : ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE UM OUTRO NÚMERO INTEIRO : ");
+            num2 = int.Parse(Console.ReadLine());
+
+            if(num1 > num2)
+                Console.WriteLine("O MAIOR NÚMERO É " + num1);
+            else if(num2 > num1)
+                Console.WriteLine("O MAIOR NÚMERO É " + num2);
+            else
+                Console.WriteLine("OS NÚMEROS SÃO IGUAIS ");
+        }
+    }
+}
---
+++ Lista02_LP3/Exercicio06/Exercicio06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2;
+
+            Console.WriteLine("DIGITE A PRIMEIRA NOTA : ");
+            nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE A SEGUNDA NOTA : ");
+            nota2 = double.Parse(Console.ReadLine());
+
+            if ((nota1 >= 0.0 && nota1 <= 10.0) && (nota2 >= 0.0 && nota2 <= 10.0))
+                Console.WriteLine("NOTAS VÁLIDAS");
+            else
+                Console.WriteLine("NOTAS INVÁLIDAS");
+        }
+    }
+}
