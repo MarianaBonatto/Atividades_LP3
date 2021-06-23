fff0877693a0d664f51ce33222bcac0703167ac9
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-06-23 14:34:16 -0300

    Add files via upload

---
+++ Lista02_LP3/Exercicio11/Exercicio11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, media, som;
+
+            Console.WriteLine("DIGITE A NOTA DO TRABALHO DE LABORATÓRIO DE 0 A 10 : ");
+            nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE A NOTA DO EXAME SEMESTRAL DE 0 A 10 : ");
+            nota2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE A TERCEIRA NOTA DO EXAME FINAL DE 0 A 10 : ");
+            nota3 = double.Parse(Console.ReadLine());
+
+            media = nota1 * 0.2 + nota2 * 0.3 + nota3 * 0.5;
+
+
+            if ((media >= 0.0) && (media <= 2.9))
+                Console.WriteLine("O ALUNO FOI REPROVADO COM " + media + " PONTOS");
+            else if ((media >= 3) && (media <= 4.9))
+                Console.WriteLine("O ALUNO ESTÁ DE RECUPERAÇÃO COM " + media + " PONTOS");
+            else 
+                Console.WriteLine("O ALUNO FOI APROVADO COM " + media + " PONTOS");
+        }
+    }
+}
---
+++ Lista02_LP3/Exercicio12/Exercicio12/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Exercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("DIGITE ENTRE 1 E 7 : ");
+            num = int.Parse(Console.ReadLine());
+
+            switch(num)
+            {
+                case 1:
+                    Console.WriteLine("DOMINGO");
+                    break;
+
+                case 2:
+                    Console.WriteLine("SEGUNDA-FEIRA");
+                    break;
+
+                case 3:
+                    Console.WriteLine("TERÇA-FEIRA");
+                    break;
+
+                case 4:
+                    Console.WriteLine("QUARTA-FEIRA");
+                    break;
+
+                case 5:
+                    Console.WriteLine("QUINTA-FEIRA");
+                    break;
+
+                case 6:
+                    Console.WriteLine("SEXTA-FEIRA");
+                    break;
+
+                case 7:
+                    Console.WriteLine("SÁBADO");
+                    break;
+            }
+        }
+    }
+}
