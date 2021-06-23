51795fbed8d4d2e2797bad61ea741fdf0920ba0a
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-06-23 14:35:12 -0300

    Add files via upload

---
+++ Lista02_LP3/Exercicio13/Exercicio13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, basema, baseme, alt;
+
+            Console.WriteLine("DIGITE A ALTURA DO TRAPÉZIO : ");
+            alt = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE A BASE MAIOR DO TRAPÉZIO : ");
+            basema = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE A BASE MENOR DO TRAPÉZIO : ");
+            baseme = double.Parse(Console.ReadLine());
+
+            if (basema > 0 && baseme > 0)
+            {
+                area = (((basema + baseme) * alt) / 2);
+                Console.WriteLine("A ÁREA DESSE TRAPÉZIO TEM O VALOR DE " + area);
+            }
+            else
+                Console.WriteLine("IMPOSSÍVEL CALCULAR, DIGITE VALORES MAIORES QUE ZERO");
+        }
+    }
+}
---
+++ Lista02_LP3/Exercicio14/Exercicio14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double l1, l2, l3;
+
+            Console.WriteLine("DIGITE O LADO 01 : ");
+            l1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE O LADO 02 : ");
+            l2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE O LADO 03 : ");
+            l3 = double.Parse(Console.ReadLine());
+
+            if ((l1 <= 0) || (l2 <= 0) || (l3 <= 0))
+                Console.WriteLine("OS VALORES INDICADOS NÃO FORMAM UM TRIÂNGULO");
+            else if ((l1 == l2) && (l1 == l3) && (l2 == l3))
+                Console.WriteLine("OS VALORES INFORMADOS FORMAM UM TRIÂNGULO EQUILÁTERO");
+            else if ((l1 == l2) || ((l1 == l3) || (l2 == l3)))
+                Console.WriteLine("OS VALORES INFORMADOS FORMAM UM TRIÂNGULO ISÓCELES");
+            else if ((l1 != l2) && (l1 != l3) && (l2 != l3)) 
+                     Console.WriteLine("OS VALORES INFORMADOS FORMAM UM TRIÂNGULO ESCALENO");
+        }
+    }
+}
