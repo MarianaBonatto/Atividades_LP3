befcf3bdd2766ec8b657ea6e1375328928ea1f1e
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-06-23 14:29:51 -0300

    Add files via upload

---
+++ Lista02_LP3/Exercicio03/Exercicio03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("DIGITE UM NÚMERO INTEIRO : ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+                Console.WriteLine("O NÚMERO " + num + " É PAR");
+            else
+                Console.WriteLine("O NÚMERO " + num + " É ÍMPAR");
+        }
+    }
+}
---
+++ Lista02_LP3/Exercicio04/Exercicio04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, res;
+
+            Console.WriteLine("DIGITE UM NÚMERO INTEIRO : ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE UM OUTRO NÚMERO INTEIRO : ");
+            num2 = int.Parse(Console.ReadLine());
+
+            if(num1 > num2)
+            {
+                res = num1 - num2;
+                Console.WriteLine("O MAIOR NÚMERO É " + num1);
+                Console.WriteLine("A DIFERENÇA DELES É " + res);
+            } else
+            {
+                res = num2 - num1;
+                Console.WriteLine("O MAIOR NÚMERO É " + num2);
+                Console.WriteLine("A DIFERENÇA DELES É " + res);
+            }
+        }
+    }
+}
