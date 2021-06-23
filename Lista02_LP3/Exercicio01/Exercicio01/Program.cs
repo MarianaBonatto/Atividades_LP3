a28c8cc585da53b27c6eaffaf61ce7c058604061
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-06-23 14:28:54 -0300

    Add files via upload

---
+++ Lista02_LP3/Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("DIGITE UM NÚMERO INTEIRO : ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE OUTRO NÚMERO : ");
+            num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+                Console.WriteLine("O MAIOR NÚMERO DIGITADO É " + num1);
+            else
+                Console.WriteLine("O MAIOR NÚMERO DIGITADO É  " + num2);
+        }
+    }
+}
---
+++ Lista02_LP3/Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num, quadrado, raiz;
+
+            Console.WriteLine("DIGITE UM NÚMERO : ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num > 0)
+            {
+                quadrado = num * num;
+                raiz = Math.Sqrt(num);
+
+                Console.WriteLine("O QUADRADO DO NÚMERO " + num + " É " + quadrado);
+                Console.WriteLine("A RAIZ QUADRADA DE " + num + " É " + raiz);
+            }
+        }
+    }
+}
