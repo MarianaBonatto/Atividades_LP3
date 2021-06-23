c85f1ff8365358bc86949fc4d78ac71de08839c3
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-06-23 14:36:15 -0300

    Add files via upload

---
+++ Lista02_LP3/Exercicio15/Exercicio15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade, tempo;
+
+            Console.WriteLine("DIGITE SUA IDADE : ");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE SEU TEMPO DE TRABALHO: ");
+            tempo = int.Parse(Console.ReadLine());
+
+            if (((idade >= 65) || (tempo >= 35)) || ((idade >= 60) && (tempo >= 25)))
+                Console.WriteLine("VOCÊ JÁ PODE DE APOSENTAR");
+            else
+                Console.WriteLine("VOCÊ AINDA NÃO PODE SE APOSENTAR");
+        }
+    }
+}
