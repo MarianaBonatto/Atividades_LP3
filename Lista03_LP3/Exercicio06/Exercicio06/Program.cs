858189086e6c17d2b41109001bc27eba31215742
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-07-02 16:44:21 -0300

    Add files via upload

---
+++ Lista03_LP3/Exercicio06/Exercicio06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ma = 0, me = 0, num = 0;
+
+            do
+            {
+                Console.WriteLine("DIGITE UM NÚMERO POSITIVO OU UM NEGATIVO PARA ENCERRAR O PROGRAMA : ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num > ma)
+                    ma = num;
+
+                if (num < ma && num >= 0)
+                    me = num;
+
+            }
+            while (num >= 0);
+            Console.WriteLine("FIM DO PROGRAMA");
+            Console.WriteLine("O MAIOR NÚMERO DIGITADO FOI " + ma);
+            Console.WriteLine("O MENOR NÚMERO DIGITADO FOI " + me);
+        }
+    }
+}
---
+++ Lista03_LP3/Exercicio07/Exercicio07/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  n = 1, num = 0;
+
+            Console.WriteLine("DIGITE UM NÚMERO POSITIVO PAR : ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            for (n = 1; n <= num; ++n)
+            {
+                if (n % 2 == 0)
+                    Console.WriteLine(n);
+                else if (num % 2 != 0)
+                { 
+                    Console.WriteLine("O NÚMERO DIGITADO NÃO É UM NÚMERO PAR");
+                    break;
+                }
+            }
+            Console.WriteLine("FIM DO PROGRAMA");
+        }
+    }
+}
