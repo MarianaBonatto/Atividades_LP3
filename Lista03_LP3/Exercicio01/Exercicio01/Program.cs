5e83f3b144f84fa93a2a6e8bce2c4835291ef778
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-07-06 11:26:30 -0300

    Add files via upload

---
+++ Lista03_LP3/Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+            Console.WriteLine("OS 5 PRIMEIROS MÚLTIPLOS DE 3 SÃO : ");
+            for (n = 0; n <= 15; ++n)
+            {
+                if(n %3 == 0 && n > 0)
+                Console.WriteLine(n);
+            }
+            Console.WriteLine("FIM DO PROGRAMA");
+        }
+    }
+}
