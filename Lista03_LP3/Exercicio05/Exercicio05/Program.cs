5c14a5388bd926b81234f35fa08710904c06aa70
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-07-12 12:39:42 -0300

    Add files via upload

---
+++ Lista03_LP3/Exercicio05/Exercicio05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0, n = 1;
+
+
+            Console.WriteLine("DIGITE UM NÚMERO INTEIRO : ");
+            num = int.Parse(Console.ReadLine());
+            n = num + 1;
+            
+            while (n >= num)
+            {
+                if ((n % 11 == 0) || (n % 13 == 0) || (n % 17 == 0))
+                {
+                    Console.WriteLine(n);
+                    break;
+                }
+                n++;
+            }
+        }
+    }
+}
