1d3c8f07fe3d11895018cf46f6ba310fcbd66940
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-07-30 15:44:26 -0300

    Add files via upload

---
+++ Aula07_LP3/Pessoa.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Aula07_LP3
+{
+    public class Pessoa
+    {
+        private string nome;
+        private string sobrenome;
+        private byte idade;
+        private float altura;
+        private float peso;
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public void setNome(string nome)
+        {
+            Console.WriteLine("Nome : " + nome);
+        }
+        public string getSobrenome()
+        {
+            return this.sobrenome;
+        }
+        public void setSobrenome(string sobrenome)
+        {
+            Console.WriteLine("Sobrenome : " + sobrenome);
+        }
+        public byte getIdade()
+        {
+            return this.idade;
+        }
+        public void setIdade(byte idade)
+        {
+            Console.WriteLine("Idade : " + idade);
+        }
+        public float getAltura()
+        {
+            return this.altura;
+        }
+        public void setAltura(float altura)
+        {
+            Console.WriteLine("Altura : " + altura);
+        }
+        public float getPeso()
+        {
+            return this.peso;
+        }
+        public void setPeso(float peso)
+        {
+            Console.WriteLine("Peso : " + peso);
+        }
+    }
+}
---
+++ Aula07_LP3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula07_LP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            p1.setNome("Dean");
+            p1.setSobrenome("Di-Laurents");
+            p1.setIdade(21);
+            p1.setAltura(1.80f);
+            p1.setPeso(82.0f);
+
+            p1.getNome();
+            p1.getSobrenome();
+            p1.getIdade();
+            p1.getAltura();
+            p1.getPeso();
+        }
+    }
+}
